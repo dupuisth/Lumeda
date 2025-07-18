bd053a1826c3809d5ca24428603f2bffbfe85fe5
Thibaut Dupuis <dupuisthibaut38@gmail.com>
2025-07-18 23:37:43 +0200

    chore: fixed glfw build

--- Lumeda/vendor/premake5.lua
+++ Lumeda/vendor/premake5.lua
@@ -1,49 +1,80 @@
 project "GLFW"
-	kind "StaticLib"
-	language "C"
-
-	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"glfw/include/GLFW/glfw3.h",
-		"glfw/include/GLFW/glfw3native.h",
-		"glfw/src/glfw_config.h",
-		"glfw/src/context.c",
-		"glfw/src/init.c",
-		"glfw/src/input.c",
-		"glfw/src/monitor.c",
-		"glfw/src/window.c"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-		staticruntime "On"
-
-		files
-		{
-			"glfw/src/win32_init.c",
-			"glfw/src/win32_joystick.c",
-			"glfw/src/win32_monitor.c",
-			"glfw/src/win32_time.c",
-			"glfw/src/win32_thread.c",
-			"glfw/src/win32_window.c",
-			"glfw/src/wgl_context.c",
-			"glfw/src/egl_context.c",
-			"glfw/src/osmesa_context.c"
-		}
-
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    kind "StaticLib"
+    language "C"
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "glfw/include/GLFW/glfw3.h",
+        "glfw/include/GLFW/glfw3native.h",
+        "glfw/src/glfw_config.h",
+        "glfw/src/context.c",
+        "glfw/src/init.c",
+        "glfw/src/input.c",
+        "glfw/src/monitor.c",
+        "glfw/src/vulkan.c",
+        "glfw/src/window.c",
+        "glfw/src/platform.c",
+        "glfw/src/null_init.c",
+        "glfw/src/null_joystick.c",
+        "glfw/src/null_monitor.c",
+        "glfw/src/null_window.c"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
+        files
+        {
+            "glfw/src/win32*.c",
+            "glfw/src/wgl_context.c",
+            "glfw/src/egl_context.c",
+            "glfw/src/osmesa_context.c"
+        }
+        defines 
+        { 
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
+    
+    -- Not tested
+    filter "system:linux"
+        files
+        {
+            "glfw/src/x11_init.c",
+            "glfw/src/x11_monitor.c",
+            "glfw/src/x11_window.c",
+            "glfw/src/xkb_unicode.c",
+            "glfw/src/posix_time.c",
+            "glfw/src/posix_thread.c",
+            "glfw/src/glx_context.c",
+            "glfw/src/egl_context.c",
+            "glfw/src/osmesa_context.c",
+            "glfw/src/linux_joystick.c"
+        }
+        defines { "_GLFW_X11" }
+    
+    -- Not tested
+    filter "system:macosx"
+        files
+        {
+            "glfw/src/cocoa_init.m",
+            "glfw/src/cocoa_joystick.m",
+            "glfw/src/cocoa_monitor.m",
+            "glfw/src/cocoa_window.m",
+            "glfw/src/cocoa_time.c",
+            "glfw/src/posix_thread.c",
+            "glfw/src/nsgl_context.m",
+            "glfw/src/egl_context.c",
+            "glfw/src/osmesa_context.c"
+        }
+        defines { "_GLFW_COCOA" }
+    
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+    
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
