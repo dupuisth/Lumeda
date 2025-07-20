9d84c45010bf3be70a50f8fbc82c79509359024f
Thibaut Dupuis <dupuisthibaut38@gmail.com>
2025-07-20 15:32:08 +0200

    fix: better error checking in ShaderOpenGL

--- Lumeda/src/Lumeda/Implementation/OpenGL/ShaderOpenGL.cpp
+++ Lumeda/src/Lumeda/Implementation/OpenGL/ShaderOpenGL.cpp
@@ -17,16 +17,40 @@ ShaderOpenGL::ShaderOpenGL(const std::string& name, const std::string& vertexPat
 
 	try
 	{
+		// Used for all compilation/linking status checks
+		int success;
+
+		// Compile the vertex shader
 		std::string vertexCode = FileReader::ReadFile(vertexPath);
 		const char* vertexSource = vertexCode.c_str();
 		glShaderSource(vertexShader, 1, &vertexSource, nullptr);
 		glCompileShader(vertexShader);
+		// Check compilation
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+		if (!success) 
+		{
+			char infoLog[InfoLogSize];
+			glGetShaderInfoLog(vertexShader, InfoLogSize, nullptr, infoLog);
+			LUMEDA_CORE_ERROR("Vertex shader compilatop, failed: {0}", infoLog);
+			throw std::runtime_error("Vertex shader compilation failed");
+		}
 
+		// Compile the fragment shader
 		std::string fragmentCode = FileReader::ReadFile(fragmentPath);
 		const char* fragmentSource = fragmentCode.c_str();
 		glShaderSource(fragmentShader, 1, &fragmentSource, nullptr);
 		glCompileShader(fragmentShader);
+		// Check compilation
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			char infoLog[InfoLogSize];
+			glGetShaderInfoLog(fragmentShader, InfoLogSize, nullptr, infoLog);
+			LUMEDA_CORE_ERROR("Fragment shader compilatop, failed: {0}", infoLog);
+			throw std::runtime_error("Fragment shader compilation failed");
+		}
 
+		// Attach the shaders then link the program
 		glAttachShader(m_Handle, vertexShader);
 		glAttachShader(m_Handle, fragmentShader);
 		glLinkProgram(m_Handle);
@@ -41,10 +65,19 @@ ShaderOpenGL::ShaderOpenGL(const std::string& name, const std::string& vertexPat
 			LUMEDA_CORE_ERROR("Failed to link program ", infoLog);
 			throw std::runtime_error("Failed to link program");
 		}
+
+		// The shaders are linked to the program, they can now be deleted
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
 	}
 	catch (std::exception& e)
 	{
 		LUMEDA_CORE_ERROR("Failed to create shader", e.what());
+
+		// If an exception is thrown, then the shaders are not cleaned up
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+
 		throw e;
 	}
 }
