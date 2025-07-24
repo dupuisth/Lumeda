dadf34d455d5ebb4e9b2f3ae2880f829c803566b
Thibaut Dupuis <dupuisthibaut38@gmail.com>
2025-07-25 00:55:05 +0200

    feat: added more uniforms setter

--- Lumeda/src/Lumeda/Renderer/Shader.h
+++ Lumeda/src/Lumeda/Renderer/Shader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 #include <string>
 
 namespace Lumeda
@@ -12,8 +14,14 @@ namespace Lumeda
 		virtual void Bind() = 0;
 		virtual void UnBind() = 0;
 
-		virtual void SetFloat(const std::string& uniform, float value) = 0;
-		virtual void SetInt(const std::string& uniform, int value) = 0;
+		virtual void SetUniform(const std::string& uniform, int value) = 0;
+		virtual void SetUniform(const std::string& uniform, float value) = 0;
+		virtual void SetUniform(const std::string& uniform, const glm::vec2& value) = 0;
+		virtual void SetUniform(const std::string& uniform, const glm::vec3& value) = 0;
+		virtual void SetUniform(const std::string& uniform, const glm::vec4& value) = 0;
+		virtual void SetUniform(const std::string& uniform, const glm::mat2& value) = 0;
+		virtual void SetUniform(const std::string& uniform, const glm::mat3& value) = 0;
+		virtual void SetUniform(const std::string& uniform, const glm::mat4& value) = 0;
 
 		virtual const std::string& GetName() const = 0;
 	};
