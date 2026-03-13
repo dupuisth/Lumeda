7fd67d94a62010ab070fa7775e278fc026bd77bf
Thibaut Dupuis <dupuisthibaut38@gmail.com>
2026-03-13 12:39:34 +0100

    feat: handled error cases for allocator

--- Lumeda/src/Lumeda/Core/Allocator.cpp
+++ Lumeda/src/Lumeda/Core/Allocator.cpp
@@ -3,7 +3,6 @@
 using namespace Lumeda;
 
 #include <Lumeda/Core/Base.h>
-
 #include <cstdlib>
 #include <new>
 
@@ -11,26 +10,37 @@ using namespace Lumeda;
 #include <malloc.h>
 #endif
 
-void* Lumeda::EngineAllocRaw(std::size_t size,
-    const char* file,
-    int line)
+void* Lumeda::EngineAllocRaw(std::size_t size, const char* file, int line)
 {
     void* ptr = malloc(size);
+    if (ptr == nullptr)
+    {
+        // Failed to allocate the given ammount
+        LUMEDA_CORE_ERROR("[Allocator] allocation failed for size={0}", size);
+        return ptr;
+    }
+    // Allocation success
     LUMEDA_PROFILE_MEMORY_ALLOC(ptr, size);
     return ptr;
 }
 
 void* Lumeda::EngineReAllocRaw(void* ptr, std::size_t size, const char* file, int line)
 {
-    // For now, lie to the profiler
-    LUMEDA_PROFILE_MEMORY_FREE(ptr);
-    ptr = realloc(ptr, size);
-    LUMEDA_PROFILE_MEMORY_ALLOC(ptr, size);
-    return ptr;
+    void* newPtr = realloc(ptr, size);
+    if (newPtr == nullptr)
+    {
+        LUMEDA_PROFILE_MEMORY_FREE(ptr);
+        LUMEDA_PROFILE_MEMORY_ALLOC(newPtr, size);
+        return newPtr;
+    }
+
+    // Failed to reallocate, the old pointer still exists
+    LUMEDA_CORE_ERROR("[Allocator] realloc failed for size={0}", size);
+    return nullptr;
 }
 
 void Lumeda::EngineFreeRaw(void* p)
 {
-    free(p);
     LUMEDA_PROFILE_MEMORY_FREE(p);
+    free(p);
 }
